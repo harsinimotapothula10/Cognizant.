29044fdb745ce95eb6a6113fd10e05d14c664059
harsinimotapothula10 <harsinimotapothula@gmail.com>
2026-07-21 19:32:07 +0530

    Add files via upload

---
+++ DeepSkilling/Week - 5/React/3. ReactJS-HOL/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+
+  return (
+
+    <div>
+
+      <CalculateScore
+
+        name="Harshini"
+
+        school="Vignan University"
+
+        total={480}
+
+        goal={6}
+
+      />
+
+    </div>
+
+  );
+
+}
+
+export default App;
